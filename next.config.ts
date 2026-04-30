a9aab226402c1857bdc3b43b422318aad79c9328
Sandeep M <723msfree@gmail.com>
2026-04-30 10:12:35 +0000

    nvalid src prop (https://miro.medium.com/v2/resize:fit:640/format:webp/1

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'miro.medium.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
